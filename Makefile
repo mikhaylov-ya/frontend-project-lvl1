63a90422e9060a2097cf2e86b4672d609f00ce75
Yaroslav Mikhaylov <sponaproz@gmail.com>
2021-12-20 19:33:01 +0300

    fourth step: set eslint

--- Makefile
+++ Makefile
@@ -6,3 +6,8 @@ brain-games:
 
 publish: 
 	npm publish --dry-run
+
+lint:
+	npx eslint .
+fix: 
+	npx eslint --fix .
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { greetings } from './cli.js';
+import greetings from './cli.js';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 greetings();
